d399ee713664504f018a67a49254f26fdea992ae
jelemiekun - pc <jeremieeden1946@gmail.com>
2025-07-13 12:22:21 +0800

    init

--- SDL_TEMPLATE/main.cpp
+++ SDL_TEMPLATE/main.cpp
@@ -1,30 +1,6 @@
-#include "Game.h"
-#include "FPSManager.h"
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
 
 int SDL_main(int argc, char* argv[]) {
-    Game* game = Game::getInstance();
-
-    game->initialize();
-
-    if (game->isRunning())
-        game->tutorial();
-
-    int countFrame = 0;
-    Uint32 startTime = SDL_GetTicks();
-
-    while (game->isRunning()) {
-        Uint32 frameStart = SDL_GetTicks();
-
-        game->processInput();
-        game->update();
-        game->render();
-
-        FPSManager::limitFPS(frameStart);
-        FPSManager::calculateAverageFPS(countFrame, startTime);
-    }
-
-    game->reset();
-
-
     return 0;
 }
